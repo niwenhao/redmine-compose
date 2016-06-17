27b70693adb781d55a058496fc0a116ed77731c5
Wenhao Ni <niwenhao@gmail.com>
2016-06-17 21:58:14 +0900

    up

--- mysql/create_redminedb.sql
+++ mysql/create_redminedb.sql
@@ -1,4 +1,4 @@
-create database redmine_production charater set = utf8;
+create database redmine_production character set = utf8;
 create user redmine identified by 'password';
 grant all on redmine_production.* to 'redmine'@'%';
 
